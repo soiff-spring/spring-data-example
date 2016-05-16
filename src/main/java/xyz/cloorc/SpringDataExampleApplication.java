d419f9f9ecda8aad60eec0e105a028fda33f2af4
zhangh <zhangh@dtdream.com>
2016-05-16 09:03:20 +0800

    initialize by spring initializr

---
+++ src/main/java/xyz/cloorc/SpringDataExampleApplication.java
@@ -0,0 +1,12 @@
+package xyz.cloorc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataExampleApplication.class, args);
+	}
+}
